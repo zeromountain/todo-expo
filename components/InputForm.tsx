02d8324f2462b1e06b72f8697203d846cdaa28b1
zeromountain <sonyeongsan@gmail.com>
2022-12-29 17:49:37 +0900

    feat: 🎸add TODO slice

--- components/InputForm.tsx
+++ components/InputForm.tsx
@@ -5,23 +5,33 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
 } from 'react-native';
-import {
-  Icon,
-  IconButton,
-  Input,
-  KeyboardAvoidingView,
-  Pressable,
-  Text,
-  View,
-} from 'native-base';
+import { Input, KeyboardAvoidingView, Pressable, View } from 'native-base';
 
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { AntDesign } from '@expo/vector-icons';
+import { useAppDispatch } from '../store/hoook';
+import { addTodo } from '../store/todos/todoSlice';
 
 const InputForm = () => {
   const { top, bottom } = useSafeAreaInsets();
+  const textRef = useRef<string>('');
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = () => {
+    if (!textRef.current.trim()) {
+      Alert.alert('할 일을 입력하세요.');
+      return;
+    }
+
+    dispatch(
+      addTodo({
+        text: textRef.current,
+      })
+    );
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -36,12 +46,13 @@ const InputForm = () => {
               backgroundColor: 'white',
               borderColor: 'black',
             }}
+            onChangeText={(text) => (textRef.current = text)}
             InputRightElement={
               <Pressable
                 hitSlop={10}
                 pressRetentionOffset={100}
                 marginRight='5px'
-                onPress={() => Alert.alert('추가추가')}>
+                onPress={handleSubmit}>
                 <AntDesign name='pluscircle' size={24} color='black' />
               </Pressable>
             }
